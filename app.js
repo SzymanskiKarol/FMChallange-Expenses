33a04a17ce9ef1f94b8272c17f9fa61911660aff
SzymanskiKarol <szymanski.karol.1991@gmail.com>
2023-01-18 14:32:05 +0100

    data from json uploaded to columns

--- app.js
+++ app.js
@@ -1,3 +1,31 @@
 import data from './data.json' assert {type: 'json'};
 
-console.log(data[0].day);
+// console.log(data[0].day);
+
+const amountSpan = document.querySelectorAll('.amount');
+const columnDiv = document.querySelectorAll('.column');
+
+amountSpan.forEach((amount, index) => {
+    amount.innerHTML = "$" + data[index].amount
+})
+
+columnDiv.forEach((column, index) => {
+    let multipler = 200 / getMax(data).max;
+    column.style.height = multipler * data[index].amount + "px";
+})
+
+columnDiv[getMax(data).maxIndex].style.background = "var(--Cyan)";
+
+
+function getMax(data) {
+    let maxArr = []
+    data.forEach((column) => {
+        maxArr.push(column.amount)
+    })
+    let max = Math.max(...maxArr);
+    let maxIndex = maxArr.indexOf(Math.max(...maxArr));
+    return {
+        "max": max,
+        "maxIndex": maxIndex,
+    };
+}
